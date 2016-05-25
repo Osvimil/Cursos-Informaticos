ab81c1eef3f6eb8d02bca2beccea9a1f192001d3
oswaldo <oswaldoadidas@hotmail.com>
2016-05-25 16:05:45 -0500

    more fragments were modifiede in order to have a great application

--- app/src/main/java/com/videumcorp/desarrolladorandroid/cursosinformaticosconandroid/Fragments/CursosBasicos.java
+++ app/src/main/java/com/videumcorp/desarrolladorandroid/cursosinformaticosconandroid/Fragments/CursosBasicos.java
@@ -5,17 +5,41 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.videumcorp.desarrolladorandroid.cursosinformaticosconandroid.R;
 
-/**
- * Created by oswaldosaldivar on 24/05/16.
- */
+
 public class CursosBasicos extends Fragment {
+
+    public  CursosBasicos(){}
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        final View view = inflater.inflate(R.layout.cursos_basicos, container, false);
+
+        View view= inflater.inflate(R.layout.cursos_basicos, container, false);
+        String[] menuItems = {"PYTHON","RUBY","GO","C++","C#","JAVA","JAVASCRIPT","PHP","CSS3","HTML5","F#","MONGODB","SQL SERVER","ANGULARJS"};
+        ListView listview = (ListView)view.findViewById(R.id.lista_basico);
+
+        ArrayAdapter<String> array = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                menuItems
+        );
+        listview.setAdapter(array);
+
+
         return view;
+
+
+
+
+
+
     }
+
+
+
 }
--- app/src/main/java/com/videumcorp/desarrolladorandroid/cursosinformaticosconandroid/MainActivity.java
+++ app/src/main/java/com/videumcorp/desarrolladorandroid/cursosinformaticosconandroid/MainActivity.java
@@ -26,10 +26,15 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     ActionBar actionBar;
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
