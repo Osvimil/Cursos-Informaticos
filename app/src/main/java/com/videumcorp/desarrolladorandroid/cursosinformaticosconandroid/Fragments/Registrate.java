51f0bf1b22c8184f7a76f95531760fee36944100
oswaldo <oswaldoadidas@hotmail.com>
2016-05-24 21:09:23 -0500

    Se agrego frameLayout del formulario de registro sin BD enlazada

--- app/src/main/java/com/videumcorp/desarrolladorandroid/cursosinformaticosconandroid/Fragments/Registrate.java
+++ app/src/main/java/com/videumcorp/desarrolladorandroid/cursosinformaticosconandroid/Fragments/Registrate.java
@@ -5,39 +5,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
-import com.videumcorp.desarrolladorandroid.cursosinformaticosconandroid.MainActivity;
 import com.videumcorp.desarrolladorandroid.cursosinformaticosconandroid.R;
 
 
 public class Registrate extends Fragment {
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.fragment_registrate, container, false);
-
-        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Inbox");
-
-        Button buttonChangeText = (Button) view.findViewById(R.id.buttonFragmentStarred);
-
-        final TextView textViewStarredFragment = (TextView) view.findViewById(R.id.textViewStarredFragment);
-
-        buttonChangeText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                textViewStarredFragment.setTextSize(40);
-                textViewStarredFragment.setText("This is the Starred Fragment");
-                textViewStarredFragment.setTextColor(getResources().getColor(R.color.md_green_800));
-
-            }
-        });
-
         return view;
     }
 
-}
+
+    }
+
+
